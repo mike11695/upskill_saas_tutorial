6be6df339006942f175ec2087b7208881367df48
Michael Lopez <luigixmario11695@gmail.com>
2017-01-14 23:59:47 +0000

    Fixed Pro Membership Registrations and remmbered to save them this time.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,6 @@
 /* global $, Stripe */
 //Document ready.
-$(document).ready(function(){
+$(document).on('turbolinks:load', function(){
   var theForm = $('#pro_form');
   var submitBtn = $('#form-signup-btn');
   
@@ -9,7 +9,6 @@ $(document).ready(function(){
   
   //When user clicks form submit btn,
   submitBtn.click(function(event){
-    
     //prevent default submission behavior.
     event.preventDefault();
     submitBtn.val("Processing").prop('disabled', true);
@@ -27,49 +26,38 @@ $(document).ready(function(){
     if(!Stripe.card.validateCardNumber(ccNum)) {
       error = true;
       alert('The credit card number appears to be invalid');
-      
     }
     
     //Validate CVC number.
     if(!Stripe.card.validateCVC(cvcNum)) {
       error = true;
       alert('The CVC number appears to be invalid');
-      
     }
     
     //Validate expiration date.
     if(!Stripe.card.validateExpiry(expMonth, expYear)) {
       error = true;
       alert('The expiration date appears to be invalid');
-      
     }
-    
+        
     if (error) {
-      
       //If there are card errors, don't send to Stripe.
       submitBtn.prop('disabled', false).val("Sign Up");
-      
-    } 
-    else {
-      
+    } else {
       //Send the card info to Stripe.
-      Stripe.card.createToken({
+      Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
         exp_month: expMonth,
         exp_year: expYear
       }, stripeResponseHandler);
-      
     }
     
     return false;
-    
   });
   
   //Stripe will return a card token.
-  
   function stripeResponseHandler(status, response) {
-    
     //Get the token from the response.
     var token = response.id;
     
@@ -78,6 +66,5 @@ $(document).ready(function(){
     
     //Submit form to our Rails app.
     theForm.get(0).submit();
-    
   }
 });
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,2 @@
-require "stripe"
-Stripe.api_version = '2016-07-06'
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
